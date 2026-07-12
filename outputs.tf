77c9d42c673d4360810cc4edbcdd0928f711efeb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "subscriptions" {
-  description = "All subscription resources"
-  value       = azurerm_subscription.subscriptions
-}
 output "subscriptions_alias" {
-  description = "List of alias values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.alias]
+  description = "Map of alias values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.alias }
 }
 output "subscriptions_billing_scope_id" {
-  description = "List of billing_scope_id values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.billing_scope_id]
+  description = "Map of billing_scope_id values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.billing_scope_id }
 }
 output "subscriptions_subscription_id" {
-  description = "List of subscription_id values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.subscription_id]
+  description = "Map of subscription_id values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.subscription_id }
 }
 output "subscriptions_subscription_name" {
-  description = "List of subscription_name values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.subscription_name]
+  description = "Map of subscription_name values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.subscription_name }
 }
 output "subscriptions_tags" {
-  description = "List of tags values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.tags]
+  description = "Map of tags values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.tags }
 }
 output "subscriptions_tenant_id" {
-  description = "List of tenant_id values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.tenant_id]
+  description = "Map of tenant_id values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.tenant_id }
 }
 output "subscriptions_workload" {
-  description = "List of workload values across all subscriptions"
-  value       = [for k, v in azurerm_subscription.subscriptions : v.workload]
+  description = "Map of workload values across all subscriptions, keyed the same as var.subscriptions"
+  value       = { for k, v in azurerm_subscription.subscriptions : k => v.workload }
 }
 
